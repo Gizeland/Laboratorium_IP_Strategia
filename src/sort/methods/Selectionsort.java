11c6cb977f810f883ddbabb12ff16a73c399b3b3
Gizeland <dominika.giza@onet.com.pl>
2021-06-17 09:14:37 +0200

    Algorytm selectionsort

--- src/sort/methods/Selectionsort.java
+++ src/sort/methods/Selectionsort.java
@@ -10,4 +10,20 @@ package sort.methods;
  * @author LeopardProMK
  */
 public class Selectionsort {
+    private static int tablica[];
+    private static int ile_liczb;
+ 
+    private static void selectionsort(int tablica[], int ile_liczb) {
+    int min,i,j,temp;
+ 
+    for (i=0;i<ile_liczb-1;i++)  {
+        min=i;
+        for (j=i+1;j<ile_liczb;j++)
+            if (tablica[j]<tablica[min])
+            min=j;
+            temp=tablica[min];
+            tablica[min]=tablica[i];
+            tablica[i]=temp;
+        }
+    }
 }
