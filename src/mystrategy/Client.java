087e3cba559f80c3b7137d9feb5ab8e53f77addd
Gizeland <dominika.giza@onet.com.pl>
2021-06-17 13:04:33 +0200

    Odpowiedzi na pytania

--- src/mystrategy/Client.java
+++ src/mystrategy/Client.java
@@ -6,6 +6,10 @@
 package mystrategy;
 
 import data.DataGenerator;
+import sort.factory.ISortStrategyFactory;
+import sort.factory.SortStrategyFactory;
+import sort.methods.ISortStrategy;
+import sort.utils.SortStrategyType;
 
 /**
  *
@@ -24,8 +28,20 @@ public class Client {
         double[] dataNonSort = DataGenerator.generate(100000);
         
        /* Wzorzec Stratega */
-        //...
        
-        System.out.println("Time: ??");
+       //Odpowiedzi na pytania:
+       //1. Wzorzec Strategia proponuje ekstrakcję poszczególnych 
+       //algorytmów wykonujących dane zadanie na różne sposoby i umieszczenie
+       //ich w odrębnych klasach, zwanych strategiami. Każde rozszerzenie algorytmu można
+       //można realizować poprzez dodanie startegii.
+       //2. Tak w trakcie trwania programu możliwa jest zmiana algorytmu.
+       //3.Kontekst wzorca strategii wywoluje metode sortowania poprzez stworzoną abstrakcje (ISortStrategy)
+       //4. Stosuje się w przypadku potrzeby roznej implementacji rozwiazania problemu np 
+       //rozszerzanie aplikacji wyznaczającej trasę do przejechania samochodem, 
+       //o wyznaczanie trasy wędrówki pieszej czy komunikacją miejską.
+        ISortStrategyFactory factory = new SortStrategyFactory();
+        ISortStrategy strategy = factory.createStrategy(SortStrategyType.Quicksort);
+        
+        System.out.println("Time: ");
     }
 }
