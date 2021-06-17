fea2f934bcc88c870b1a6981712883f8a3896c00
Gizeland <dominika.giza@onet.com.pl>
2021-06-17 08:10:14 +0200

    Algorytm wstawiania

--- src/sort/methods/Insertionsort.java
+++ src/sort/methods/Insertionsort.java
@@ -10,4 +10,25 @@ package sort.methods;
  * @author LeopardProMK
  */
 public class Insertionsort {
+    public static void insertionSort(int[] tablica){
+ 
+        int klucz, j;
+ 
+        //dla kazdego elementu tablicy do posortowania, poczawszy od drugiego
+        for (int i=1;i<tablica.length;i++){
+        j=i;
+        klucz=tablica[i];
+            while (j>0 && tablica[j-1]>klucz){
+            tablica[j]=tablica[j-1];
+            j--;
+        }
+        tablica[j]=klucz;
+        }
+ 
+//wypisz zawartosc posortowanej tablicy
+    for(int i = 0 ; i<tablica.length; i++){
+        System.out.println("#"+ (i+1) +" :\t" + tablica[i]);
+    }
+        System.out.println();
+    }
 }
